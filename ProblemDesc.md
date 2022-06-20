6b5dd309361f00e7df56f8c5bb0e596b1e0459b4
sklanier <stevekentlanier@gmail.com>
2022-06-20 16:25:34 -0500

    🚀 READY: solved with Abdullahi

--- ProblemDesc.md
+++ ProblemDesc.md
@@ -1,17 +1,25 @@
 Let's say that an array follows an alternating parity pattern if it contains no two consecutive integers of the same parity (in other words, neither two consecutive even numbers nor two consecutive odd numbers).
+
 Given an array of non-negative integers numbers, find if it contains elements that break the alternating parity pattern.
+
 Return an index of the first element where the alternating parity pattern breaks, or -1 if there is no such element.
+
 Example
 
     For numbers = [1, 2, 5, 3, 6], the output should be solution(numbers) = 3.
+
     Since both numbers[2] = 5 and numbers[3] = 3 are odd, the first element where the alternating parity pattern breaks is numbers[3].
+
     For numbers = [1, 4, 7, 2, 5, 6], the output should be solution(numbers) = -1.
+
     All elements in even-numbered positions (numbers[0] = 1, numbers[2] = 7 and numbers[4] = 5) are odd, and all elements in odd-numbered positions (numbers[1] = 4, numbers[3] = 2 and numbers[5] = 6) are even, therefore there is no elements where pattern breaks.
 
 Input/Output
 
     [execution time limit] 4 seconds (js)
+
     [input] array.integer numbers
+
     An array of integers.
 
     Guaranteed constraints:
@@ -21,3 +29,13 @@ Input/Output
     [output] integer
 
     Return an index of the first element where the alternating parity pattern breaks, or -1 if there is no such element.
+
+[JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+    console.log("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+
--- parityAlterationBreaks.js
+++ parityAlterationBreaks.js
@@ -15,7 +15,49 @@ Tests
      [28969601, 441869524, 436734027, 80539398, 110861487, 134300596, 342673051, 509389018, 623484865, 482540390] --> -1
 
 */
+const t1 = [1, 2, 5, 3, 6];
+const t2 = [1, 4, 7, 2, 5, 6];
+const t3 = [0];
+const t4 = [100, 100, 100, 100];
+const t5 = [50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40];
+const t6 = [51, 52, 53, 54, 60];
+const t7 = [1000000000, 1, 900000000, 2, 3, 4, 5];
+const t8 = [972423194, 776100481, 736859790, 844690425, 823033043, 944853776, 971324293, 162355436];
+const t9 = [278987595, 723712500, 181868340, 911972022, 971678045, 109002520, 936442861, 351571714, 615190353, 972198082];
+const t10 = [28969601, 441869524, 436734027, 80539398, 110861487, 134300596, 342673051, 509389018, 623484865, 482540390];
 
 function solution(numbers){
-    
+    //iterate over the array
+    //sliding window technique
+    for (let i = 0; i < numbers.length - 1; i++){
+        //if the first number is even, the second number should be odd
+        if (numbers[i] % 2 == 0){
+            if (numbers[i+1] % 2 != 0){
+                continue
+            }
+            else{
+                return i + 1
+            }
+        }else if(numbers[i] % 2 != 0){
+            if (numbers[i+1] % 2 == 0){
+                continue
+            }
+            else{
+                return i + 1
+            }
+        }
+        //if the first number is odd, the second number should be even
+    }
+    return -1
 }
+
+console.log(solution(t1))
+console.log(solution(t2))
+console.log(solution(t3))
+console.log(solution(t4))
+console.log(solution(t5))
+console.log(solution(t6))
+console.log(solution(t7))
+console.log(solution(t8))
+console.log(solution(t9))
+console.log(solution(t10))
